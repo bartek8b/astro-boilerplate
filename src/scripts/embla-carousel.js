3646502c0fb1b98f22f71822dafc59a60757576f
bartek8b <163062773+bartek8b@users.noreply.github.com>
2026-04-15 23:46:07 +0200

    Delete commented parts of code for false looping

--- src/scripts/embla-carousel.js
+++ src/scripts/embla-carousel.js
@@ -6,25 +6,13 @@ const viewportNode = wrapperNode.querySelector('.embla__viewport');
 const prevButtonNode = wrapperNode.querySelector('.embla__prev');
 const nextButtonNode = wrapperNode.querySelector('.embla__next');
 
-const emblaApi = EmblaCarousel(viewportNode, { loop: true, align: 'center' }, [
-  Autoplay(),
-]);
+const emblaApi = EmblaCarousel(
+  viewportNode,
+  { loop: true, align: 'center' },
+  [Autoplay()],
+);
 
 prevButtonNode.addEventListener('click', () => emblaApi.goToPrev(), false);
 nextButtonNode.addEventListener('click', () => emblaApi.goToNext(), false);
-
-// Use when loop is false to set button inactive
-
-// const toggleButtonsDisabled = (emblaApi) => {
-//   const setButtonState = (button, enabled) => {
-//     button.toggleAttribute('disabled', !enabled);
-//   };
-//   setButtonState(prevButtonNode, emblaApi.canScrollPrev());
-//   setButtonState(nextButtonNode, emblaApi.canScrollNext());
-// };
-
-// toggleButtonsDisabled(emblaApi);
-// emblaApi.on('select', toggleButtonsDisabled);
-// emblaApi.on('reInit', toggleButtonsDisabled);
 
 emblaApi.plugins().autoplay?.play();
