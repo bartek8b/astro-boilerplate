75772c27bf079871980647380a73da04ecddd380
bartek8b <163062773+bartek8b@users.noreply.github.com>
2026-04-15 12:14:13 +0200

    Add script to update current page in nav

---
+++ src/scripts/nav-current.js
@@ -0,0 +1,19 @@
+function updateNavCurrent() {
+  const links = document.querySelectorAll('header nav a');
+  const current = window.location.pathname;
+
+  links.forEach((link) => {
+    const href = link.getAttribute('href');
+    if (href === current || (href !== '/' && current.startsWith(href))) {
+      link.classList.add('current');
+    } else {
+      link.classList.remove('current');
+    }
+  });
+}
+
+// After first load
+updateNavCurrent();
+
+// After every View Transitions
+document.addEventListener('astro:after-swap', updateNavCurrent);
