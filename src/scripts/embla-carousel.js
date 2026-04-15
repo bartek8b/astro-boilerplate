f07eea850e25919a4dc3c91e89585a576d9d6ed0
bartek8b <163062773+bartek8b@users.noreply.github.com>
2026-04-15 23:08:58 +0200

    Set commented code for non looping carousel

--- src/scripts/embla-carousel.js
+++ src/scripts/embla-carousel.js
@@ -13,4 +13,18 @@ const emblaApi = EmblaCarousel(viewportNode, { loop: true, align: 'center' }, [
 prevButtonNode.addEventListener('click', () => emblaApi.goToPrev(), false);
 nextButtonNode.addEventListener('click', () => emblaApi.goToNext(), false);
 
+// Use when loop is false to set button inactive
+
+// const toggleButtonsDisabled = (emblaApi) => {
+//   const setButtonState = (button, enabled) => {
+//     button.toggleAttribute('disabled', !enabled);
+//   };
+//   setButtonState(prevButtonNode, emblaApi.canScrollPrev());
+//   setButtonState(nextButtonNode, emblaApi.canScrollNext());
+// };
+
+// toggleButtonsDisabled(emblaApi);
+// emblaApi.on('select', toggleButtonsDisabled);
+// emblaApi.on('reInit', toggleButtonsDisabled);
+
 emblaApi.plugins().autoplay?.play();
