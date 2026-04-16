c5ba3d28ffe6b6714a43fe3cb1aecf84cbb1a9bb
bartek8b <163062773+bartek8b@users.noreply.github.com>
2026-04-16 18:33:34 +0200

    Implementation of dot buttons based on Embla documentation

--- src/scripts/embla-carousel.js
+++ src/scripts/embla-carousel.js
@@ -5,12 +5,70 @@ const wrapperNode = document.querySelector('.embla');
 const viewportNode = wrapperNode.querySelector('.embla__viewport');
 const prevButtonNode = wrapperNode.querySelector('.embla__prev');
 const nextButtonNode = wrapperNode.querySelector('.embla__next');
+const dotsNode = wrapperNode.querySelector('.embla__dots');
 
 const emblaApi = EmblaCarousel(viewportNode, { loop: true, align: 'center' }, [
   Autoplay(),
 ]);
 
-prevButtonNode.addEventListener('click', () => emblaApi.goToPrev(), false);
-nextButtonNode.addEventListener('click', () => emblaApi.goToNext(), false);
+prevButtonNode.addEventListener(
+  'click',
+  () => {
+    emblaApi.plugins().autoplay?.stop();
+    emblaApi.scrollPrev();
+  },
+  false,
+);
+nextButtonNode.addEventListener(
+  'click',
+  () => {
+    emblaApi.plugins().autoplay?.stop();
+    emblaApi.scrollNext();
+  },
+  false,
+);
+
+let dotNodes = [];
+
+const createDotButtonHtml = (emblaApi, dotsNode) => {
+  const dotTemplate = wrapperNode.querySelector('#embla-dot-template');
+  const snapList = emblaApi.scrollSnapList();
+  dotsNode.innerHTML = snapList.reduce(
+    (acc) => acc + dotTemplate.innerHTML,
+    '',
+  );
+  return Array.from(dotsNode.querySelectorAll('.embla__dot'));
+};
+
+const addDotButtonClickHandlers = (emblaApi, dotNodes) => {
+  dotNodes.forEach((dotNode, index) => {
+    dotNode.addEventListener(
+      'click',
+      () => {
+        emblaApi.plugins().autoplay?.stop();
+        emblaApi.scrollTo(index);
+      },
+      false,
+    );
+  });
+};
+
+const toggleDotButtonsActive = (emblaApi, dotNodes) => {
+  if (!dotNodes.length) return;
+  const previous = emblaApi.previousScrollSnap();
+  const selected = emblaApi.selectedScrollSnap();
+  dotNodes[previous].classList.remove('embla__dot--selected');
+  dotNodes[selected].classList.add('embla__dot--selected');
+};
+
+const createAndSetupDotButtons = (emblaApi, dotsNode) => {
+  dotNodes = createDotButtonHtml(emblaApi, dotsNode);
+  addDotButtonClickHandlers(emblaApi, dotNodes);
+  toggleDotButtonsActive(emblaApi, dotNodes);
+};
+
+createAndSetupDotButtons(emblaApi, dotsNode);
+emblaApi.on('reInit', () => createAndSetupDotButtons(emblaApi, dotsNode));
+emblaApi.on('select', (emblaApi) => toggleDotButtonsActive(emblaApi, dotNodes));
 
 emblaApi.plugins().autoplay?.play();
