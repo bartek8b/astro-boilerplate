5f771aa04b09d2fb20d20c2a7f303b2161f5776d
bartek8b <163062773+bartek8b@users.noreply.github.com>
2026-04-17 15:56:42 +0200

    Fix: add trailing slash to base site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   // Change to target URL when implementing a real project
-  site: 'https://site-name.domain',
+  site: 'https://site-name.domain/',
   integrations: [sitemap()],
 });
