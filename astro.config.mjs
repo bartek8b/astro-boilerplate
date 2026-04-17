44f99638411fbc32a6bdb24accfeca151e813b96
bartek8b <163062773+bartek8b@users.noreply.github.com>
2026-04-17 15:53:42 +0200

    Add sitemap integration for dinamic map creation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // Change to target URL when implementing a real project
+  site: 'https://site-name.domain',
+  integrations: [sitemap()],
+});
