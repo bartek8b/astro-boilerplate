94ed31e44291edfab3f32ff264b76281822a92e4
bartek8b <163062773+bartek8b@users.noreply.github.com>
2026-06-10 21:08:55 +0200

    chore: add node adapter, configure gitkeep for content layer and document configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
 import keystatic from '@keystatic/astro';
 import mdx from '@astrojs/mdx';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,4 +14,9 @@ export default defineConfig({
     prefetchAll: true,
     defaultStrategy: 'hover',
   },
+  // The Node adapter is required for Keystatic's API routes to function (both in dev and build).
+  // Feel free to swap it for your deployment platform's adapter (e.g., Cloudflare, Vercel, Netlify).
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
