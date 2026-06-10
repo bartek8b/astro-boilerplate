f0c841f703af3e876ceeda6985ce2f20c9df84a0
bartek8b <163062773+bartek8b@users.noreply.github.com>
2026-06-10 20:46:53 +0200

    Add output: 'hybrid' to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ export default defineConfig({
   // Change to target URL when implementing a real project
   site: 'https://site-name.domain/',
   integrations: [sitemap(), react(), keystatic(), mdx()],
+  output: 'hybrid',
   prefetch: {
     prefetchAll: true,
     defaultStrategy: 'hover',
